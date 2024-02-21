8ae58f95af0f0ddc63c273dbe6dc410f472654ce
hardresets29 <147424859+hardresets29@users.noreply.github.com>
2024-02-21 14:03:06 +0100

    Paper fix

--- components/MainMenuItem.tsx
+++ components/MainMenuItem.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { type MainMenuItemProps } from "@/types";
-import { Menu } from "@mui/material";
+import { Menu, Paper } from "@mui/material";
 import Link from "next/link";
-import { useRef, useState, type ComponentProps } from "react";
+import {
+  forwardRef,
+  useRef,
+  useState,
+  type ComponentProps,
+  type Ref,
+} from "react";
 import SubMenuItem from "./SubMenuItem";
 
 export default function MainMenuItem({
@@ -35,6 +41,16 @@ export default function MainMenuItem({
           }}
           slots={{
             root: (props: ComponentProps<"div">) => <div {...props} />,
+            paper: forwardRef(
+              (
+                props: ComponentProps<typeof Paper>,
+                ref: Ref<HTMLDivElement>,
+              ) => (
+                <div className="absolute pt-4">
+                  <Paper ref={ref} {...props} className="border" />
+                </div>
+              ),
+            ),
           }}
         >
           {submenu.map(({ href, children }, key) => (
